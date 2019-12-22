f5a15b13c32147513c256006f9467f0145e07ec7
sd1700092 <foreverskyone@hotmail.com>
2019-12-22 15:29:55 +0800

    change write to writeAndFlush

--- src/main/java/nettyinaction/chapter2/EchoClientHandler.java
+++ src/main/java/nettyinaction/chapter2/EchoClientHandler.java
@@ -1,7 +1,6 @@
 package nettyinaction.chapter2;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
@@ -13,12 +12,12 @@ public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
     
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ctx.write(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
     }
     
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf in) throws Exception {
-        System.out.println("Client received: " + ByteBufUtil.hexDump(in.readBytes(in.readableBytes())));
+        System.out.println("Client received: " +in.toString(CharsetUtil.UTF_8));
     }
     
     @Override
